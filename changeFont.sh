39423f1903076691adfb1760fe3c5ef5ba9b960b
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2022-07-14 23:49:24 -0400

    Update changeFont.sh

--- changeFont.sh
+++ changeFont.sh
@@ -10,3 +10,10 @@ sudo fc-cache -fv
 fc-cache -fv
 
 echo "you need to reboot your pc"
+
+#Mohammed Besar
+#https://www.youtube.com/c/mmbesar
+#https://mastodon.online/mbesar
+#https://twitter.com/MMBesar
+#https://www.facebook.com/Mohammed.Besar.Page
+#https://www.instagram.com/mmbesar
